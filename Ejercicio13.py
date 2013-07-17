123d744424683785b9b5a2b6e3e4bd2ff514e534
elyex <github@elyex.com>
2013-07-17 16:04:06 -0500

    2

--- Ejercicio13.py
+++ Ejercicio13.py
@@ -4,7 +4,7 @@
 #  Twitter: @MauEyx
 #  Facebook: https://www.facebook.com/MauEyx
 #--------------------------------------------
-#  Programa: Imagenes Movimiento con el teclado
+#  Programa: Imagenes Movimiento con el teclado iz y dr
 #--------------------------------------------
 
 import pygame #importar modulo Pygame
@@ -12,11 +12,28 @@ pygame.init() #Inicia las librerias de pygame
 pantalla=pygame.display.set_mode((480,300)) 
 salir=False
 reloj1=pygame.time.Clock()
+imagen1=pygame.image.load("monster.png")
+(x,y) = (100,100)
+vx=0
+r1=pygame.Rect(250,80,10,400)
 while salir!=True: #Loop principal del juego 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			salir=True			
-		reloj1.tick(25)
-		pantalla.fill((255,255,255))
-		pygame.display.update()
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_LEFT:
+				vx-=10
+			if event.key == pygame.K_RIGHT:
+				vx+=10
+		if event.type == pygame.KEYUP:
+			if event.key == pygame.K_LEFT:
+				vx=0
+			if event.key == pygame.K_RIGHT:
+				vx=0
+	x+=vx
+	reloj1.tick(15)
+	pantalla.fill((255,255,255))
+	pantalla.blit(imagen1,(x,y))
+	pygame.draw.rect(pantalla,(0,0,0),r1)
+	pygame.display.update()
 pygame.quit()
